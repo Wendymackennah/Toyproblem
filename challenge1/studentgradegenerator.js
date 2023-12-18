8fdf987aba750b11b7de84414bc1bc778b4c9a8e
wendymackennah <wendymackennah5@gmail.com>
2023-12-18 11:36:36 +0300

    first commit

---
+++ challenge1/studentgradegenerator.js
@@ -0,0 +1,11 @@
+const consoleInput = prompt("Enter Student Marks");
+
+function calculateGrade (marks){
+    if (marks>=79 && marks <=100){
+        return 'A';
+    }
+    else if (marks >=59 &&  marks <=69){
+        return 'B';
+    } else if (marks>=49  && marks<=59)
+
+} 
---
+++ studentgradegenerator.js
@@ -0,0 +1,20 @@
+const consoleInput = prompt("Enter Student Marks");
+
+function calculateGrade(marks){
+    if (marks>=79 && marks <=100){
+        return 'A';
+    }
+    else if (marks >=60 &&  marks <=69){
+        return 'B';
+    } else if (marks>=49 && marks<=59){
+        return 'C';
+    }else if(marks>=40 && marks<=49){
+        return 'D';
+    }else if(marks >=40){
+        return 'E';
+    }else{
+        return 'Invalid input';
+    }
+} 
+const grade=calculateGrade(parseInt(consoleInput));
+alert('Your student marks: ${consoleInput}\n Grade:$(grade)');
